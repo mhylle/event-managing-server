70ab8ec616350b66a3c377b891ae23e26346e942
mhylle <mhylle@yahoo.com>
2016-04-09 00:17:05 +0200

    password are now salted and hashes and store as the hash

--- users/user.js
+++ users/user.js
@@ -2,13 +2,12 @@
  * Created by mah on 05-04-2016.
  */
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
+var bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     id: {type: String, unique: true, required: true},
     username: {type: String, unique: true, required: true},
     hash: {type: String},
-    salt: {type: String},
     firstname: {type: String, required: true},
     middlename: {type: String},
     lastname: {type: String, required: true},
@@ -21,55 +20,32 @@ var UserSchema = new mongoose.Schema({
 
 UserSchema.pre('save', function (next, req, callback) {
     var user = this;
-
-    bcrypt.genSalt(36, function (error, salt) {
+    var password = req.body.password;
+    bcrypt.hash(password, 8, function (error, hash) {
         if (error) {
             return callback(error);
         }
 
-        var password = req.body.password;
-        bcrypt.hash(password, salt, null, function (error, hash) {
-            if (error) {
-                return callback(error);
-            }
-
-            user.hash = hash;
-            user.salt = salt;
-            next(callback);
-        });
+        user.hash = hash;
+        next(callback);
     });
-
-    // callback();
-    // todo ensure any stuff that needs to be done before a save is done here..
 });
 
 UserSchema.methods.verifyPassword = function (password, callback) {
     var user = this;
     var userhash = user.hash;
-    var salt = user.salt;
 
     if (userhash === undefined) {
         return callback('no userhash, something is wrong');
     }
 
-    bcrypt.hash(password, salt, null, function (error, hash) {
+    bcrypt.compare(password, userhash, function (error, result) {
         if (error) {
-            return callback(null, false);
+            callback(null, false);
+        } else {
+            callback(null, result);
         }
-        console.log('hash:    ' + hash);
-        console.log('userhash ' + userhash);
-        console.log('should they be the same? ' + hash === userhash);
-        bcrypt.compare(hash, userhash, function (error, result) {
-            if (error) {
-                callback(null, false);
-            } else {
-                callback(null, result);
-            }
-        });
-
     });
-    // var compareSync = bcrypt.compareSync(hash, userhash);
-    // callback(null, compareSync);
 };
 
 module.exports = mongoose.model('User', UserSchema);
