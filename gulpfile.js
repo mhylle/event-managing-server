4f2d316e9941f249eb65c63ec57b5d5d867ef44a
mhylleberg@csc.com <mhylle@yahoo.com>
2016-04-11 11:05:02 +0200

    added gulp and start mongod scripts

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mah on 11-04-2016.
+ */
+var gulp = require('gulp');
+var exec = require('child_process').exec;
+
+function runCommand(command) {
+    return function (cb) {
+        exec(command, function (err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+            cb(err);
+        });
+    }
+}
+
+//Running mongo
+//http://stackoverflow.com/a/28048696/46810
+gulp.task('start-mongo', runCommand('mongod --dbpath ./data/'));
+gulp.task('stop-mongo', runCommand('mongo --eval "use admin; db.shutdownServer();"'));
+gulp.task('start-app', runCommand('node index.js'));
