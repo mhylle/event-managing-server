437ec2c85f45178161335677118c1b00a0108a61
mhylle <mhylle@yahoo.com>
2016-05-17 07:49:15 +0200

    added header stuff for cors

--- index.js
+++ index.js
@@ -49,7 +49,9 @@ app.use(bodyparser.urlencoded({
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers, common");
     next();
 });
 
