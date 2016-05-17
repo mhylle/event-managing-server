0aef44481adbd6170f5b1362d05cb122453e78a0
mhylleberg@csc.com <mhylle@yahoo.com>
2016-05-17 11:04:03 +0200

    now sending back proper status codes on login

--- security/login.controller.js
+++ security/login.controller.js
@@ -7,12 +7,12 @@ var UUID = require('../utils/uuid').generate();
 exports.login = function (req, res, next) {
     var authorization = req.headers['authorization'];
     if (!authorization) {
-        return res.json({message: 'Must supply credentials'});
+        return res.status(401).json({message: 'No Authorization: Must supply credentials'});
     }
 
     var parts = authorization.split(' ');
     if (parts.length < 2) {
-        return res.json({message: 'Must supply credentials1'});
+        return res.status(401).json({message: 'Must supply credentials1'});
     }
     var credentials = new Buffer(parts[1], 'base64').toString().split(':');
     User.findOne({username: credentials[0]}, function (error, user) {
@@ -21,13 +21,13 @@ exports.login = function (req, res, next) {
         }
 
         if (!user) {
-            return res.json({message: 'User not found'});
+            return res.status(401).json({message: 'User not found'});
         }
 
         if (user.checkPassword(credentials[1])) {
-            return res.json({accessToken: UUID.generate()});
+            return res.status(200).json({accessToken: UUID.generate()});
         } else {
-            return res.json({message: 'Password did not match'});
+            return res.status(401).json({message: 'Password did not match'});
         }
     });
     // var callback = req.query.callback || '/profile';
